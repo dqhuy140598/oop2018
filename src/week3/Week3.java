f49a644ab20e934f576cb2cbf7ddac4466d10254
dqhuy140598 <huylai007>
2018-10-02 20:54:07 +0700

    update week3

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -1,15 +1,31 @@
 package week3;
 
 public class Week3 {
-
+    /**
+     * Chương trình tính số lớn hơn trong hai số nguyên
+     * @param m số nguyên thứ nhất
+     * @param n số nguyên thứ hai
+     * @return  số lớn hơn trong hai số
+     */
     public static int max(int m, int n) {
         // TODO: Tìm giá trị lớn nhất của hai số nguyên, giá trị trả về của hàm là số lớn nhất
-        return -1;
+        return Math.max(m,n);
     }
 
+    /**
+     * chương trình tìm số nhỏ nhất trong mảng số nguyên.
+     * @param array mảng số nguyên
+     * @return giá trị nhỏ nhất trong mảng
+     */
     public static int minOfArray(int[] array) {
         // TODO: Tìm giá trị nhỏ nhất của của một mảng số nguyên (kích thước mảng <= 100 phần tử)
-        return -1;
+       int min = array[0];
+       for(int i: array){
+           if(i<=min){
+               min = i;
+           }
+       }
+       return min;
     }
 
     /**
@@ -20,6 +36,13 @@ public class Week3 {
      */
     public static String calculateBMI(double weight, double height) {
         // TODO: Viết chương trình tính chỉ số BMI và in ra kết quả đánh giá
-        return null;
+        double BMI = weight / (Math.pow(height,2));
+        if(BMI<18.5)    return "Thiếu cân";
+        else if(BMI>=18.5 && BMI<=22.99)    return "Bình thường";
+        else if(BMI >= 23 && BMI<=24.99)    return "Thừa cân";
+        return "Bèo phì";
+    }
+    public static void main(String Args []){
+
     }
 }
--- test/week3/Week3Test.java
+++ test/week3/Week3Test.java
@@ -1,10 +1,47 @@
 package week3;
 
-public class Week3Test {
+import org.junit.Assert;
+public class Week3Test{
 
     // TODO: Viết 5 testcase cho phương thức max()
 
+    /**
+     * Chương trình tạo 5 test case cho hàm max trong Week3
+     */
+    public void testMax(){
+        Assert.assertEquals(5,Week3.max(5,2));
+        Assert.assertEquals(1000,Week3.max(1000,8));
+        Assert.assertEquals(15,Week3.max(15,6));
+        Assert.assertEquals(555,Week3.max(555,-10));
+        Assert.assertEquals(54,Week3.max(45,54));
+    }
     // TODO: Viết 5 testcase cho phương thức minOfArray()
 
+    /**
+     * Chương trình tạo 5 test case cho hàm minOfArray trong Week3
+     */
+    public void testMinArray(){
+        int[] A = {1,2,3,4,5};
+        int[] B ={4,6,3,-1,7,9,11};
+        int[] C ={18,56,2,44,50000,623};
+        int[] D ={123,-1,7,7777,999999,5544};
+        int[] E ={1,1,1,1,1,1,1,1,1,1};
+        Assert.assertEquals(1,Week3.minOfArray(A));
+        Assert.assertEquals(-1,Week3.minOfArray(B));
+        Assert.assertEquals(2,Week3.minOfArray(C));
+        Assert.assertEquals(-1,Week3.minOfArray(D));
+        Assert.assertEquals(1,Week3.minOfArray(E));
+    }
     // TODO: Viết 5 testcase cho phương thức calculateBMI()
+
+    /**
+     * Chương trình tạo 5 test case cho hàm caculateBMI trong week3
+     */
+    public void testBMI(){
+        Assert.assertEquals("Thiếu cân",Week3.calculateBMI(42,1.7));
+        Assert.assertEquals("Bình thường",Week3.calculateBMI(61,1.7));
+        Assert.assertEquals("Thừa cân",Week3.calculateBMI(66,1.65));
+        Assert.assertEquals("Bèo phì",Week3.calculateBMI(80,1.68));
+        Assert.assertEquals("Bình thường",Week3.calculateBMI(67,1.81));
+    }
 }
