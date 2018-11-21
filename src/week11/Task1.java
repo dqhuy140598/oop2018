3928d45f2eb72eabb1155cf3ceeaff9aa259945c
dqhuy140598 <huylai007>
2018-11-21 10:14:23 +0700

    update week11

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,28 @@
 package week11;
 
 public class Task1 {
+    public <T extends Comparable<T>> void Sort(T[] Arr){
+        for(int i=0;i<Arr.length-1;i++){
+            for(int j=i+1;j<Arr.length;j++){
+                if(Arr[i].compareTo(Arr[j])>0){
+                    T temp = Arr[j];
+                    Arr[j]= Arr[i];
+                    Arr[i] = temp;
+                }
+            }
+        }
+    }
+    public <T>void printArr(T[] Arr){
+        for(int i=0;i<Arr.length;i++){
+            System.out.print(Arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        Integer Arr[] = {4,5,6,1,2,3};
+        Double Brr[] = {1.3,1.5,6.7,8.9,11.3};
+        Task1 task1 = new Task1();
+        task1.<Integer>Sort(Arr);
+        task1.<Integer>printArr(Arr);
+    }
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,24 @@
 package week11;
 
+import java.util.ArrayList;
+
 public class Task2 {
+    public <T extends Comparable<T>> T MaxElement(ArrayList<T> Arr){
+        T Max = Arr.get(0);
+        for(int i=0;i<Arr.size();i++){
+            if(Arr.get(i).compareTo(Max)>0){
+                Max = Arr.get(i);
+            }
+        }
+        return Max;
+    }
+    public static void main(String Args[]){
+        ArrayList<Integer> Arr = new ArrayList<Integer>();
+        Arr.add(4);
+        Arr.add(6);
+        Arr.add(7);
+        Task2 task2 = new Task2();
+        System.out.println(task2.MaxElement(Arr));
+    }
+
 }
